40812e4f0f63d62f54cbf5f7f3365dec5e48f5a8
a <a@aui-MacBookAir.local>
2022-08-19 12:41:40 +0900

    ADD: 회원가입, 로그인 기능추가 두번째 리뷰 후 수정본

--- utils/validation.js
+++ utils/validation.js
@@ -2,15 +2,21 @@ const validateEmailPw = (email, password) => {
     const emValidation = new RegExp(
         /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/
     );
+    if(!emValidation.test(email)){
+        const err = new Error("EMAIL_NOT_AVAILABLE")
+        err.statusCode = 400;
+        throw err;
+        }
 
     const pwValidation = new RegExp(
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,16}$/
     );
-
-   if (!emValidation.test(email) || !pwValidation.test(password)) {
-        const err = new Error("EMAIL_OR_PASSWORD_NOT_AVAILABLE");
+    
+    if (!pwValidation.test(password)) {
+        const err = new Error("PASSWORD_NOT_AVAILABLE");
         err.statusCode = 400;
         throw err;
+   
     }
 };
       
