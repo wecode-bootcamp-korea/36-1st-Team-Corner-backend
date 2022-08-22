a7288519bdc9817d841ca4824c23fc990f5720e1
LeeJ1Hyun <nextlinehappy516@gmail.com>
2022-08-22 13:14:19 +0900

    Add: REVIEWS 권한 검사 기능 추가, 함수명 일치화, 엔드포인트 수정

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const reviewController = require("../controllers/reviewController");
 const auth = require("../middlewares/auth");
+const commonAuth = require("../utils/commonAuth");
 
 const router = express.Router();
 
@@ -8,6 +9,7 @@ router.post("/:productId/review", auth.validationToken, reviewController.postRev
 router.patch("/:productId/review", auth.validationToken, reviewController.patchReview);
 router.delete("/:productId/review", auth.validationToken, reviewController.deleteReview);
 router.get("/:productId/reviews", reviewController.getReviewList);
+router.get("/:productId/review/access",commonAuth.commonAuth);
 
 module.exports = {
     router
