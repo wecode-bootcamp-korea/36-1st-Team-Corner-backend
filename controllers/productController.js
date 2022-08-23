2000bc2a2a5b4e4e17bc86efa4bf9fcf0a347eb9
a <a@aui-MacBookAir.local>
2022-08-23 23:11:57 +0900

    상품조회기능 수정본6

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,7 @@
 const productService = require('../services/productService');
 
 const productList = async (req, res) => {
-    
+    try{
     const pageInfo = req.query;
     
     const page = parseInt(pageInfo.page)
@@ -9,44 +9,44 @@ const productList = async (req, res) => {
     const cate = parseInt(pageInfo.cate)
     const orderBy = pageInfo["orderBy"]
 
-    try{
-        if(!page || !pageSize){
-        return res.status(400).json({ message: 'UNKNOWN_VALUE' })
-    }
+    if(!page || !pageSize){
+        return res.status(400).json({ message: 'UNKNOWN_VALUE' })}
+    
         const list = await productService.productList(page, pageSize, cate, orderBy)
-        res.status(200).json({data: list})}
-
+        
+        res.status(200).json({data: list})
+        }
     catch (err) {console.log(err);
         return res.status(err.statusCode || 500).json({ message: err.message });
   }}
 
 const productSearch = async (req, res) => {
-    
+    try {
     const pageInfo = req.query;
     const word = pageInfo["q"]
-    console.log(word)
+    
     if (!word) {
         return res.status(400).json({ message: 'NONE_WORD' });
     }
-    try {const list = await productService.productSearch(word)
-        res.status(200).json({data: list})
+    const list = await productService.productSearch(word)
+        
+    res.status(200).json({data: list})}
 
-    }
     catch (err) {console.log(err);
         return res.status(err.statusCode || 500).json({ message: err.message });
   }}
 
 const productInformation = async (req, res) => {
     
+    try {
     const {id} = req.params
     
     if (!id) {
         return res.status(400).json({ message: 'UNKNOWN_VALUE' });
     }
-    try {const list = await productService.productInformation(id)
+    const list = await productService.productInformation(id)
         res.status(200).json({data: list})
-
-    }
+}
     catch (err) {console.log(err);
         return res.status(err.statusCode || 500).json({ message: err.message });
   }}
