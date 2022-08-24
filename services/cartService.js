f7aa397c9b7d4d4862fa93d0dc97406a4357b120
a <a@aui-MacBookAir.local>
2022-08-24 16:44:23 +0900

    카트 정보 조회 기능 수정본1

--- services/cartService.js
+++ services/cartService.js
@@ -1,7 +1,7 @@
 const cartDao = require("../models/cartDao");
 
 const getCarts = async (userId) => {
-    console.log(userId)
+  
     const getAllCart = await cartDao.getAllCart(userId)
       return getAllCart
     }
